a30a879c9d022e5fa3a739778b92e737aedde798
hugobbi <hgugobbi@gmail.com>
2023-03-30 01:16:23 -0300

    Creating edges of graph

--- bruijn.py
+++ bruijn.py
@@ -19,16 +19,40 @@ def bruijn_graph(sequence: str, k: int) -> ig.Graph:
         if len(kmer) != k:
             continue
 
+        # counts frequency of kmers and add them to dictionary
         if kmer in kmers.keys():
             kmers[kmer] += 1
         else:
             kmers[kmer] = 1
 
-    print(kmers)
+    kmers_list = kmers.keys()
+    # using a set so there's no duplicate values
+    vertices = set()
+    edges = set()
 
-    return 1
+    for kmer1 in kmers_list:
+        for kmer2 in kmers_list:
+            if kmer1 != kmer2:
+                # checks if kmers overlap each other using k-1-mers
+                if kmer1[1:] == kmer2[:-1]:
+                    edges.add((kmer1[:-1], kmer2[:-1]))
+                    vertices.add(kmer1[:-1])
+                    vertices.add(kmer2[:-1])
+                if kmer1[:-1] == kmer2[1:]:
+                    edges.add((kmer2[:-1], kmer1[:-1]))
+                    vertices.add(kmer1[:-1])
+                    vertices.add(kmer2[:-1])
 
-seq = "ATGGAAGTCGCGGAATC"
+    print(edges)
+
+    burjin_graph = ig.Graph()
+    burjin_graph.add_vertices(len(vertices))
+    burjin_graph.add_edges(edges)
+    burjin_graph.vs['label'] = vertices
+
+    return burjin_graph    
+
+seq  = "ATGGAAGTCGCGGAATC"
 k = 3
 
 seq1 = "ATC"
